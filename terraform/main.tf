a297b1e470ec627e78c6c17cc4d4f55231dc6da5
deep-18 <deepraval7408@gmail.com>
2026-03-03 20:47:22 -0500

    "Commit terraform"

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,97 @@
-resource "aws_instance" "host" {
-    ami = "ami-0b6c6ebed2801a5cb"
-    instance_type = "t3.micro"
-    tags = {
-      project = "deployment"
-    }
+# resource "aws_instance" "host" {
+#     ami = "ami-0b6c6ebed2801a5cb"
+#     instance_type = "t3.micro"
+#     tags = {
+#       project = "deployment"
+#     }
+# }
+
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+  region = "us-east-1"
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+resource "aws_subnet" "main_public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+
+resource "aws_subnet" "main_private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+resource "aws_route_table" "public_route" {
+  vpc_id = aws_vpc.example.id
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+resource "aws_route" "r1" {
+  route_table_id            = aws_route_table.public_route.id
+  destination_cidr_block    = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.main_public.id
+  route_table_id = aws_route_table.public_route.id
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "demo_instance_public" {
+  subnet_id = aws_subnet.main_public.id
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  region = "us-east-1"
+
+  tags = {
+    Name = "Main"
+  }
+}
+resource "ec2_instance" "demo_instance_private" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  tags = {
+    Name = "Main"
+  }
 }
